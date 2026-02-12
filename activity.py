17aea8ae6e40770dc1be7e468860446e3baaa736
Mebin Thattil <mail@mebin.in>
2026-02-12 23:14:37 +0530

    V1-SpeakAI Genesis
    v1 adds new AI features to speak, such as:
    * A new text-to-speech model for voice
    * A small language model to replace brain
    * An option to fetch responses from a cloud hosted LLM when connected to
      internet.
    
    This marks the start of a new activity - SpeakAI.
    
    Files updated:
    - NEWS
    - activity.info
    - activity.py (copyright update)

--- activity.py
+++ activity.py
@@ -1,26 +1,28 @@
-# Speak.activity
-# A simple front end to the espeak text-to-speech engine on the XO laptop
-# http://wiki.laptop.org/go/Speak
+# SpeakAI.activity
+# SpeakAI is a talking face. Anything you type will be spoken aloud using a text-to-speech model.
 #
 # Copyright (C) 2008  Joshua Minor
 # Copyright (C) 2014  Walter Bender (major refactoring)
-# This file is part of Speak.activity
+# Copyright (C) 2026  Mebin J Thattil (transition to SpeakAI, new TTS, SLM and LLM integration)
+# This file is part of SpeakAI.activity
+# 
 #
-# Parts of Speak.activity are based on code from Measure.activity
+# SpeakAI.activity is based on the Speak.activity
+# Parts of SpeakAI.activity are based on code from Measure.activity
 # Copyright (C) 2007  Arjun Sarwal - arjun@laptop.org
 #
-#     Speak.activity is free software: you can redistribute it and/or modify
+#     SpeakAI.activity is free software: you can redistribute it and/or modify
 #     it under the terms of the GNU General Public License as published by
 #     the Free Software Foundation, either version 3 of the License, or
 #     (at your option) any later version.
 #
-#     Speak.activity is distributed in the hope that it will be useful,
+#     SpeakAI.activity is distributed in the hope that it will be useful,
 #     but WITHOUT ANY WARRANTY; without even the implied warranty of
 #     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #     GNU General Public License for more details.
 #
 #     You should have received a copy of the GNU General Public License
-#     along with Speak.activity.  If not, see <http://www.gnu.org/licenses/>.
+#     along with SpeakAI.activity.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
 import os
